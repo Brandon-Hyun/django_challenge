03369a7121878e046f854f642695719ea49dd2d2
RandomUsername_Kr <theoriginaldongik@gmail.com>
2026-01-30 10:13:08 +0900

    day02 challenge

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_completed = models.BooleanField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
